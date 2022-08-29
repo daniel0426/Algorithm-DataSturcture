70417b84e14183c7e21861c1ca6c2490632e86c3
Daniel <angus9209@gmail.com>
2022-08-29 22:42:23 +1200

    solve some simple recursive problems

--- js/recursion.js
+++ js/recursion.js
@@ -54,4 +54,62 @@ function collectOddValues2(arr){
   return newArr
 }
 
-console.log(collectOddValues2([1,2,3,4,5,6,7,8,9,10]))
+// console.log(collectOddValues2([1,2,3,4,5,6,7,8,9,10]))
+
+
+//power
+function power(base, exp){
+  if(exp === 0 ){
+    return 1
+  }
+  return base * power(base, exp-1)
+}
+
+// console.log(power(2,3))
+
+
+
+//factorial
+function factorial(num){
+  if(num <= 1){
+    return 1
+  }
+  return num * factorial(num-1)
+}
+// console.log(factorial(3))
+
+
+//productOfArray 
+function productOfArray(arr){
+  if(arr.length === 0) {
+    return 1;
+}
+return arr[0] * productOfArray(arr.slice(1));
+}
+
+// console.log(productOfArray([1,2,3,4]))
+
+
+//recursiveRange
+function recursiveRange(num){
+  if(num === 0){
+    return 0
+  }
+  return num + recursiveRange(num-1)
+}
+// console.log(recursiveRange(6))
+
+
+//fib
+//Write a recursive function called fib which accepts a number 
+//and returns the nth number in the Fibonacci sequence. 
+//Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... 
+//which starts with 1 and 1, and where every number thereafter is 
+//equal to the sum of the previous two numbers.
+
+function fib(n){
+  if(n<=2) return 1;
+  return fib(n-1) + fib(n-2)
+}
+
+console.log(fib(10))
