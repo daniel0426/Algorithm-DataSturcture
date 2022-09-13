e9ea11c056b07b698b698160183e3fd1f99d24be
Daniel <angus9209@gmail.com>
2022-09-14 07:32:36 +1200

    implement radix sort

--- js/intermediate_sorting.js
+++ js/intermediate_sorting.js
@@ -109,4 +109,19 @@ function mostDigits(nums){
   return maxDigits
 }
 
-console.log(mostDigits([12,24,124124,12414142144,123]))
+//Radix sort 
+function radixSort(nums){
+  let maxDigitCount = mostDigits(nums);
+  for(let k=0; k < maxDigitCount; k++){
+    let digitBuckets = Array.from({length:10}, ()=> [])
+    for(let i=0; i<nums.length; i++){
+      let digit = getDigit(nums[i], k)
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
+
+console.log(radixSort([23,345,5467,12,2345,9852]))
+
