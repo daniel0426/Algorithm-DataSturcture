4d1ee987831975c1de47a2210da19339234ec567
Daniel <angus9209@gmail.com>
2022-09-13 23:34:07 +1200

    implement helper functions for radix sort

--- js/intermediate_sorting.js
+++ js/intermediate_sorting.js
@@ -85,4 +85,28 @@ return arr
   
 }
 
-console.log(quickSort([4,8,2,1,5,7,6,3]))
+// console.log(quickSort([4,8,2,1,5,7,6,3]))
+
+
+//Radix Sort
+
+//helper function 
+
+function getDigit(num, i){
+  return Math.floor(Math.abs(num)/ Math.pow(10,i)) % 10
+}
+
+function digitCount(num){
+  if(num===0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) +1
+}
+
+function mostDigits(nums){
+  let maxDigits = 0;
+  for(let i =0; i<nums.length ; i++){
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
+
+console.log(mostDigits([12,24,124124,12414142144,123]))
